8cea16bbf80ad448fc9f9736e4211f77f48483ae
paulwb <paulwb>
2009-09-09 06:31:25 +0000

    Debugger Component

---
+++ anemone/components/EventDebugger.php
@@ -0,0 +1,65 @@
+<?php
+	class EventDebugger implements IObserver, IContent
+	{
+		private $output = "";
+		
+		public function setSubject(Observable & $subject) {
+			$this->debug("\n".__CLASS__." constructed by ".get_class($subject));
+			$subject->register($this, Observable::EVENT_ALL);
+		}
+		
+		public function notify(Observable & $subject, $eventType, IEventArguments $arguments) {
+			$this->debug("Event fired by ".get_class($subject)." with arguments ".get_class($arguments));
+			$this->debug($arguments);
+		}
+		
+		private function debug($obj) {
+			$str = $this->get_string_representation($obj);
+			// echo $str."\n";
+			$this->output .= $str."\n";
+		}
+		
+		private function get_string_representation($obj, $level = 0) {
+			$str = "";
+			if(is_object($obj)) {
+				$str .= get_class($obj).":\n";
+				foreach(get_object_vars($obj) as $k => $v) {
+					if($obj == $this && $k == "output")
+						continue;
+					$str .= str_repeat("\t", $level+1)."$k->".$this->get_string_representation($v, $level+1)."\n";
+				}
+				if($obj instanceof ISettable) {
+					$ar = $obj->getAvailableProperties();
+					if(!is_array($ar)) {
+						$str .= str_repeat("\t", $level+1)."No settable attributes!";
+					} else {
+						foreach($obj->getAvailableProperties() as $k) {
+							$str .= str_repeat("\t", $level+1)."$k->".$this->get_string_representation($obj->get($k), $level+1)."\n";
+						}
+					}
+				}
+				return $str;
+			}
+			
+			if(is_array($obj)) {
+				return implode(", ", $obj);
+			}
+			
+			return $obj;
+		}
+		
+		public function setParentContent(IContent & $parent_content){}
+		
+		public function render(){
+			return $this->output;
+		}
+		
+		public function getOutput() {
+			return $this->output;
+		}
+		
+		public function getContentType() {
+			return "text/plain";
+		}
+	}
+?>
